537b6b64ac571aae31be039acec52f6b65131af2
Sebadlpc <sebadlpc@gmail.com>
2025-05-15 20:19:01 -0400

    Update Guia 2.5.2.py

--- Primera_unidad/Actividad 2.5/Guia 2.5.2.py
+++ Primera_unidad/Actividad 2.5/Guia 2.5.2.py
@@ -23,4 +23,76 @@ while sw==1:
             puntos-=30000
             print(f"Canje exitoso, le quedan {puntos} puntos")
     if op==3:
-        exit()
+        sw=0 ##Tambien se puede usar un exit() o break
+---------------------------------------------------------------------------
+sw=1
+while sw==1:
+    print("Opciones a elegir: \n"
+          "1. Ver mi saldo \n"
+          "2. Retirar dinero \n"
+          "3. Salir")
+    opcion=int(input("Seleccione una opcion: "))
+    try:
+        if(opcion >0 and opcion <4):
+            if opcion == 1:
+                print(f"Tienes un saldo de $500000")
+                op=int(input("Presione (1) para volver atras o (2) para salir: "))
+                if op==1:
+                   sw=1
+                elif op==2:
+                   print("Cierre de sesión exitoso, adiós")
+                   sw=0
+            elif opcion==2:
+                print("Transferencia relizada")
+                op=int(input("Presione (1) para volver atras o (2) para salir: "))
+                if op==1:
+                   sw=1
+                elif op==2:
+                   print("Cierre de sesión exitoso, adiós")
+                   sw=0
+            elif opcion==3:
+                print("Cierre de sesión exitoso, adiós")
+                sw=0
+            else:
+                print("Opcion fuera de rango")
+    except:
+        print("Ingreso erroneo")
+--------------------------------------------------------------------------------------------
+sw=1
+saldo=0
+while sw==1:
+    print("Opciones a elegir: \n"
+          "1. Ver mi saldo \n"
+          "2. Cargar saldo \n"
+          "3. Salir")
+    opcion=int(input("Seleccione una opcion: "))
+    try:
+        if(opcion > 0 and opcion < 4):
+            if opcion == 1:
+                print(f"Tiene un saldo de {saldo}")
+                continu = int(input("presione 1) para volver atrás, presione 2) para salir "))
+                if continu ==2:
+                    print("Cierre de sesión exitoso, adiós")
+                    sw=0
+            elif opcion == 2:
+                print("¿Cuánto desea cargar? \n"
+                      "1.- $1.000 \n"
+                      "2.- $5.000 \n"
+                      "3.- $20.000")
+                continu = int(input("Seleccione la opción: "))
+                if continu ==1:
+                    saldo = saldo+1000
+                    print(f"Carga exitosa, su saldo es de: ${saldo}")
+                elif continu ==2:
+                    saldo = saldo+5000
+                    print(f"Carga exitosa, su saldo es de: ${saldo}")
+                elif continu ==3:
+                    saldo = saldo+20000
+                    print(f"Carga exitosa, su saldo es de: ${saldo}")
+            if opcion == 3:
+                print("Muchas gracias por ocupar el servicio, adiós")
+                sw=0
+            else:
+                print("Selección fuera de rango")
+    except:
+        print("Ingreso Erróneo")
